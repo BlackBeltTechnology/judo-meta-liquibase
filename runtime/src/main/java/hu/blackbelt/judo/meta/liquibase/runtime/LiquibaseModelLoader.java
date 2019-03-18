4d127dd9d0adb499d327b4cfed061c1504cf27cc
Robert Csakany <csakany.robert@gmail.com>
2019-03-18 03:20:22 +0100

    Updating model and runtime code

---
+++ runtime/src/main/java/hu/blackbelt/judo/meta/liquibase/runtime/LiquibaseModelLoader.java
@@ -0,0 +1,80 @@
+package hu.blackbelt.judo.meta.liquibase.runtime;
+
+import hu.blackbelt.judo.meta.liquibase.LiquibasePackage;
+import hu.blackbelt.judo.meta.liquibase.util.LiquibaseResourceFactoryImpl;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.URIHandler;
+import org.eclipse.emf.ecore.resource.impl.ResourceFactoryRegistryImpl;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LiquibaseModelLoader {
+
+    public static void registerLiquibaseMetamodel(ResourceSet resourceSet) {
+        resourceSet.getPackageRegistry().put(LiquibasePackage.eINSTANCE.getNsURI(), LiquibasePackage.eINSTANCE);
+    }
+
+
+    public static Resource.Factory getLiquibaseFactory() {
+        return new LiquibaseResourceFactoryImpl();
+    }
+
+    public static ResourceSet createLiquibaseResourceSet() {
+        return createLiquibaseResourceSet(null);
+    }
+
+    public static ResourceSet createLiquibaseResourceSet(URIHandler uriHandler) {
+        ResourceSet resourceSet = new ResourceSetImpl();
+        registerLiquibaseMetamodel(resourceSet);
+        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(ResourceFactoryRegistryImpl.DEFAULT_EXTENSION, getLiquibaseFactory());
+        if (uriHandler != null) {
+            resourceSet.getURIConverter().getURIHandlers().add(0, uriHandler);
+        }
+        return resourceSet;
+    }
+
+
+    public static LiquibaseModel loadLiquibaseModel(URI uri, String name, String version) throws IOException {
+        return loadLiquibaseModel(createLiquibaseResourceSet(), uri, name, version, null, null);
+    }
+
+    public static LiquibaseModel loadLiquibaseModel(ResourceSet resourceSet, URI uri, String name, String version) throws IOException {
+        return loadLiquibaseModel(resourceSet, uri, name, version, null, null);
+    }
+
+    public static LiquibaseModel loadLiquibaseModel(ResourceSet resourceSet, URI uri, String name, String version, String checksum, String acceptedMetaVersionRange) throws IOException {
+        registerLiquibaseMetamodel(resourceSet);
+        Resource resource = resourceSet.createResource(uri);
+        Map<Object, Object> loadOptions = new HashMap<>();
+        //loadOptions.put(XMLResource.OPTION_RECORD_UNKNOWN_FEATURE, Boolean.TRUE);
+        //loadOptions.put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
+        loadOptions.put(XMLResource.OPTION_DEFER_IDREF_RESOLUTION, Boolean.TRUE);
+        loadOptions.put(XMLResource.OPTION_LAX_FEATURE_PROCESSING, Boolean.TRUE);
+        loadOptions.put(XMLResource.OPTION_PROCESS_DANGLING_HREF, XMLResource.OPTION_PROCESS_DANGLING_HREF_DISCARD);
+        resource.load(loadOptions);
+
+        LiquibaseModel.LiquibaseModelBuilder b = LiquibaseModel.buildLiquibaseModel();
+
+        b.name(name)
+                .version(version)
+                .uri(uri)
+                .checksum(checksum)
+                .resource(resource);
+
+        if (checksum != null) {
+            b.checksum(checksum);
+        }
+
+        if (acceptedMetaVersionRange != null)  {
+            b.metaVersionRange(acceptedMetaVersionRange);
+        }
+        return b.build();
+    }
+
+}
---
+++ runtime/src/test/java/hu/blackbelt/judo/meta/liquibase/LiquibaseModelLoaderTest.java
@@ -0,0 +1,39 @@
+package hu.blackbelt.judo.meta.liquibase;
+
+import hu.blackbelt.judo.meta.liquibase.runtime.LiquibaseModel;
+import hu.blackbelt.judo.meta.liquibase.runtime.LiquibaseModelLoader;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+@Slf4j
+class LiquibaseModelLoaderTest {
+
+    @Test
+    void loadLiquibaseModel() throws IOException {
+        LiquibaseModel psmModel = LiquibaseModelLoader.loadLiquibaseModel(
+                URI.createURI(new File(srcDir(), "test/models/example.changelog.xml").getAbsolutePath()),
+                "test",
+                "1.0.0");
+
+        for (Iterator<EObject> i = psmModel.getResource().getAllContents(); i.hasNext(); ) {
+            log.info(i.next().toString());
+        }
+    }
+
+    public File srcDir(){
+        String relPath = getClass().getProtectionDomain().getCodeSource().getLocation().getFile();
+        File targetDir = new File(relPath+"../../src");
+        if(!targetDir.exists()) {
+            targetDir.mkdir();
+        }
+        return targetDir;
+    }
+
+
+}

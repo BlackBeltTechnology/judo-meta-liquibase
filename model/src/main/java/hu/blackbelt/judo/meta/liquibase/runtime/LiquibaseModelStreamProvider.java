156040043a01d9d634a953b381ad4a777071c5d7
Robert Csakany <csakany.robert@gmail.com>
2021-09-11 08:13:32 +0200

    JNG-2524 Add model stream provider

--- model/src/main/java/hu/blackbelt/judo/meta/liquibase/runtime/LiquibaseModelStreamProvider.java
+++ model/src/main/java/hu/blackbelt/judo/meta/liquibase/runtime/LiquibaseModelStreamProvider.java
@@ -1,6 +1,7 @@
 package hu.blackbelt.judo.meta.liquibase.runtime;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -10,8 +11,9 @@ import java.io.InputStream;
 import static hu.blackbelt.judo.meta.liquibase.runtime.LiquibaseModel.SaveArguments.liquibaseSaveArgumentsBuilder;
 import static hu.blackbelt.judo.meta.liquibase.runtime.LiquibaseNamespaceFixUriHandler.fixUriOutputStream;
 
-@Slf4j
 public class LiquibaseModelStreamProvider {
+
+    static Logger log = LoggerFactory.getLogger(LiquibaseModelStreamProvider.class);
 
     static InputStream getStreamFromLiquibaseModel(LiquibaseModel liquibaseModel) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
